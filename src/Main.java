47c7fde95a961d839fb41226fd1dae0afb282e1b
Megumi Uza <mik.vanrgby@gmail.com>
2018-11-15 21:05:05 +0900

    prog2/report4/step2　try-catch getMessageの出力

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        String str = null;
-        System.out.println(str.length());
+
+        try {
+
+            String str = null;
+            System.out.println(str.length());
+
+
+        } catch (NullPointerException e) {
+
+            System.out.println("Error --> NullPointerException");
+
+            System.out.println(e.getMessage());
+
+
+        }
     }
 
 }
